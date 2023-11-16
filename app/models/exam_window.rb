21e57c95539c1263c725cc43f8cd56d2aca4dee7
Prasidh Prabudhan <prasidhprabudhan123@gmail.com>
2023-11-16 12:30:17 +0400

    Added validations for user, exam and exam_window

--- app/models/exam_window.rb
+++ app/models/exam_window.rb
@@ -1,3 +1,16 @@
 class ExamWindow < ApplicationRecord
   belongs_to :exam
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+
+  validate :start_date_before_end_date
+
+  private
+
+    def start_date_before_end_date
+      if start_date.present? && end_date.present? && start_date > end_date
+        errors.add(:start_date, "must be before the end date")
+      end
+    end
 end
